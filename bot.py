41d8c3f1aae854d392d26f4275b076cfec061eba
aperuchena <aimarperuchena@gmail.com>
2020-08-27 12:45:36 +0200

    change user_agent

--- bot.py
+++ bot.py
@@ -219,8 +219,7 @@ def extractMatchList(link):
 
 
 def extractMarkets(link):
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.3'}    
     reg_url = link
     req = Request(url=reg_url, headers=headers)
     html = urlopen(req).read()
