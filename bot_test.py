6a5f0c06a4b947ef4061a995bb22b257456ebbf9
aimarperuchena <aimarperuchena@gmail.com>
2020-09-29 20:59:03 +0200

    try catch

--- bot_test.py
+++ bot_test.py
@@ -64,28 +64,34 @@ time.sleep(2)
 
     
 
-soup2 = BeautifulSoup(html, "html.parser")
-date_time = ''
-date = ''
-times = ''
-game = ''
-game_id = ''
-market_id = ''
-sport_id = ''
-sport = ''
-league = ''
-league_id = ''
-'''DATE'''
-span_date = soup2.find("span", {"class": "hora dateFecha"})
-date_time = span_date['title']
-date_time = date_time.split(" ")
-date = date_time[0]
-times = date_time[1]
-times = times.replace("Z", "")
-'''GAME TEAMS'''
-game_title = soup2.find("h3", {"class": "titulo_seccion"})
-game = game_title.text
-print(game)
+try:
+
+  soup2 = BeautifulSoup(html, "html.parser")
+  date_time = ''
+  date = ''
+  times = ''
+  game = ''
+  game_id = ''
+  market_id = ''
+  sport_id = ''
+  sport = ''
+  league = ''
+  league_id = ''
+  '''DATE'''
+  span_date = soup2.find("span", {"class": "hora dateFecha"})
+  date_time = span_date['title']
+  date_time = date_time.split(" ")
+  date = date_time[0]
+  times = date_time[1]
+  times = times.replace("Z", "")
+  '''GAME TEAMS'''
+  game_title = soup2.find("h3", {"class": "titulo_seccion"})
+  game = game_title.text
+  print(game)
+except Exception as e:
+        print(e)
+time.sleep(100000)
+driver.close()
 
 driver.quit()
 
--- test.py
+++ test.py
@@ -1,29 +1,49 @@
-import mysql.connector
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from bs4 import BeautifulSoup
+from selenium.webdriver.chrome.options import Options  
 
-dbServerName = "35.242.242.84"
-dbUser = "remote"
-dbPassword = "aimar"
-dbName = "Kirolbet_db"
+chrome_options = Options()  
+chrome_options.add_argument("--headless") 
 
-mydb = mysql.connector.connect(
-  host=dbServerName,
-  user=dbUser,
-  password=dbPassword,
-  database=dbName
-)
+driver = webdriver.Chrome(executable_path='chromedriver',   chrome_options=chrome_options)  
 
-mycursor = mydb.cursor()
-""" 
-mycursor.execute("SELECT * FROM sport")
 
-myresult = mycursor.fetchall()
-for x in myresult:
-  print(x) """
+driver = webdriver.Chrome('chromedriver.exe')
+driver.maximize_window() 
+driver.get("https://euskadi.kirolbet.es/esp/Sport/Evento/2222407")
+elem = driver.find_element_by_xpath("//*")
+html = elem.get_attribute("outerHTML")
 
-sql = "INSERT INTO error (type, des) VALUES (%s, %s)"
-val = ("John", "Highway 21")
-mycursor.execute(sql, val)
 
-mydb.commit()
 
-print(mycursor.rowid, "record inserted.")
+    
+try:
+
+  soup2 = BeautifulSoup(html, "html.parser")
+  date_time = ''
+  date = ''
+  times = ''
+  game = ''
+  game_id = ''
+  market_id = ''
+  sport_id = ''
+  sport = ''
+  league = ''
+  league_id = ''
+  '''DATE'''
+  span_date = soup2.find("span", {"class": "hora dateFecha"})
+  date_time = span_date['title']
+  date_time = date_time.split(" ")
+  date = date_time[0]
+  times = date_time[1]
+  times = times.replace("Z", "")
+  '''GAME TEAMS'''
+  game_title = soup2.find("h3", {"class": "titulo_seccion"})
+  game = game_title.text
+  print(game)
+except Exception as e:
+        print(e)
+time.sleep(100000)
+driver.close()
