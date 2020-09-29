390d38660fc130502adedf98d91985508e8ddb22
aimarperuchena <aimarperuchena@gmail.com>
2020-09-29 19:26:31 +0200

    driver change

--- bot_test.py
+++ bot_test.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()  
 chrome_options.add_argument("--headless") 
 
-driver = webdriver.Chrome(executable_path='chromedriver.exe',   chrome_options=chrome_options)  
+driver = webdriver.Chrome(executable_path='/usr/local/bin/chromedriver',   chrome_options=chrome_options)  
 
 
 """driver = webdriver.Chrome('chromedriver.exe')
