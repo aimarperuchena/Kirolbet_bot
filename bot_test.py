8f417e0de4d3ae68be00526eac762ea803d8e0e8
aimarperuchena <aimarperuchena@gmail.com>
2020-09-29 19:44:32 +0200

    path change

--- bot_test.py
+++ bot_test.py
@@ -7,7 +7,7 @@ from selenium.webdriver.chrome.options import Options
 chrome_options = Options()  
 chrome_options.add_argument("--headless") 
 
-driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver',   chrome_options=chrome_options)  
+driver = webdriver.Chrome(executable_path='chromedriver_linux',   chrome_options=chrome_options)  
 
 
 """driver = webdriver.Chrome('chromedriver.exe')
