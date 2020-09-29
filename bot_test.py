a9b343ac583a8bbbd5052fdcf18761e57851a079
aimarperuchena <aimarperuchena@gmail.com>
2020-09-29 20:46:59 +0200

    driver quit

--- bot_test.py
+++ bot_test.py
@@ -55,6 +55,7 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('/usr/bin/chromedriver',options=chrome_options)
+driver.quit()
 
  
 
