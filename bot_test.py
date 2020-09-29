cf39d80863b4a03589b7476e782556dd1fd696bd
aimarperuchena <aimarperuchena@gmail.com>
2020-09-29 20:37:09 +0200

    test code

--- bot_test.py
+++ bot_test.py
@@ -47,8 +47,13 @@ time.sleep(100000)
 driver.close() """
 
 
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 from selenium import webdriver
-driver = webdriver.Firefox()
-driver.get('http://google.com')
-print(driver.title)
-driver.quit()
+options = FirefoxOptions()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
+driver.get("https://pythonbasics.org")
+
+ 
+
+
