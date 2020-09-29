3038ef8fe6970422df400caa8c0de6520773ba14
aimarperuchena <aimarperuchena@gmail.com>
2020-09-29 20:53:41 +0200

    kirolbet add test

--- bot_test.py
+++ bot_test.py
@@ -55,6 +55,37 @@ chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('/usr/bin/chromedriver',options=chrome_options)
+driver.get("https://euskadi.kirolbet.es/esp/Sport/Evento/2222407")
+elem = driver.find_element_by_xpath("//*")
+html = elem.get_attribute("outerHTML")
+
+
+
+    
+
+soup2 = BeautifulSoup(html, "html.parser")
+date_time = ''
+date = ''
+times = ''
+game = ''
+game_id = ''
+market_id = ''
+sport_id = ''
+sport = ''
+league = ''
+league_id = ''
+'''DATE'''
+span_date = soup2.find("span", {"class": "hora dateFecha"})
+date_time = span_date['title']
+date_time = date_time.split(" ")
+date = date_time[0]
+times = date_time[1]
+times = times.replace("Z", "")
+'''GAME TEAMS'''
+game_title = soup2.find("h3", {"class": "titulo_seccion"})
+game = game_title.text
+print(game)
+
 driver.quit()
 
  
