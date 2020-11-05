9eb0dd6d087553ead8b887b216b904764f46862e
aimarperuchena <aimarperuchena@gmail.com>
2020-11-05 19:14:58 +0100

    add bots

--- launch_creator.py
+++ launch_creator.py
@@ -1,5 +1,5 @@
 cont=1
-for x in range(1, 4000, 300):
+add for x in range(1, 6300, 300):
     call='xterm  -T BOT_'+str(cont)+' -e python3 bot_params.py '+str(x)+' '+str(x+100)+' & '
     print('xterm  -T BOT_'+str(cont)+' -e python3 bot_params.py '+str(x)+' '+str(x+100)+' & ')
     with open('launch2.sh', 'a') as the_file:
