d62a0624127c6311f4b2d268c8ca12cca0433856
aimarperuchena <aimarperuchena@gmail.com>
2020-11-28 17:00:53 +0100

    start new test

--- latest/start.py
+++ latest/start.py
@@ -1,7 +1,4 @@
 import os
-cont=1
-for x in range(0, 200, 16):
-    command='python3 new_bot.py '+str(x)+' '+str(x+15)
-    os.system(command)
+command='python3 new_bot.py 0 15 & python3 new_bot.py 16 31 & python3 new_bot.py 32 47 & python3 new_bot.py 48 63 &  python3 new_bot.py 64 79 &  python3 new_bot.py 80 95 & python3 new_bot.py 96 111 & python3 new_bot.py 112 127 & python3 new_bot.py 128 143 & python3 new_bot.py 144 159 & python3 new_bot.py 160 175 & python3 new_bot.py 176 191 & python3 new_bot.py 192 207 & '
+os.system(command)
     
-    cont=cont+1
