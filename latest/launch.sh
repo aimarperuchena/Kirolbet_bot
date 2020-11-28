822b1486eff321c7e2010214abd3a7062ca69bc5
aimarperuchena <aimarperuchena@gmail.com>
2020-11-28 16:48:29 +0100

    add bin bash

--- latest/launch.sh
+++ latest/launch.sh
@@ -1,1 +1,2 @@
+ #!/bin/bash
  python3 new_bot.py 0 15 & python3 new_bot.py 16 31 & python3 new_bot.py 32 47 & python3 new_bot.py 48 63 &  python3 new_bot.py 64 79 &  python3 new_bot.py 80 95 & python3 new_bot.py 96 111 & python3 new_bot.py 112 127 & python3 new_bot.py 128 143 & python3 new_bot.py 144 159 & python3 new_bot.py 160 175 & python3 new_bot.py 176 191 & python3 new_bot.py 192 207 & 
